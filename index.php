6b1b94f8c09264564b4e01ac0559dc066f65eae1
raydanielg <airezra2+1@gmail.com>
2025-10-25 11:10:55 +0300

    commit

--- index.php
+++ index.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // The path to your Laravel project directory (one level up from public_html)
-$project_path = __DIR__ . '/../rsms';
+$candidates = [
+    __DIR__,
+    dirname(__DIR__),
+    __DIR__ . '/../rsms',
+    dirname(__DIR__) . '/rsms',
+    '/home/amoleckc/rsms',
+];
+
+$project_path = null;
+foreach ($candidates as $path) {
+    if (is_file($path . '/vendor/autoload.php') && is_file($path . '/bootstrap/app.php')) {
+        $project_path = $path;
+        break;
+    }
+}
+
+if (!$project_path) {
+    http_response_code(500);
+    echo 'Base path not found. Ensure the Laravel project (with vendor and bootstrap) exists.';
+    exit;
+}
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = $project_path . '/storage/framework/maintenance.php')) {
